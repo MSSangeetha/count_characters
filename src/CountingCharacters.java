d9671f41063d110a087674a740eba8bcb6eb7ccf
Sangeetha <sangeethasanthosh.m@gmail.com>
2022-01-01 16:35:23 -0600

    updated the code to search for a string in a string

--- src/CountingCharacters.java
+++ src/CountingCharacters.java
@@ -11,19 +11,22 @@ public class CountingCharacters {
 //        Create an instance of the class
         CountingCharacters countingC = new CountingCharacters();
 
-//        call the function
-        System.out.println(countingC.charCount('b', "big fat bubble"));
+//        call the function to find the number of times the letter is present in String
+        System.out.println("The letter count is " + countingC.charCount('b', "big fat bubble"));
+
+//        call the function to find number of times a string present in another string
+        System.out.println("The string count is " + countingC.stringCount("aa","kaaaaple"));
     }
 
     public int charCount(char a, String b){
-int count = 0;
+    int count = 0;
 
 //converting the string to char array
 char[] charArray = b.toCharArray();
 
 for (int i=0;i<charArray.length;i++){
     if(charArray[i] == a){
-count++;
+    count++;
     }
 }
 //        if(b.contains(a)){
@@ -32,5 +35,25 @@ count++;
 //        }
         return count;
 
+    }
+
+    public int stringCount(String c, String d){
+        int times = 0;
+
+        int index = d.indexOf(c);
+//        System.out.println(index);
+
+        while(index!=-1){
+            times++;
+            d = d.substring(index+2,d.length());
+            System.out.println("The substring is " + d);
+            index = d.indexOf(c);
+        }
+
+//        if(d.contains(c)){
+//            times++;
+//        }
+
+        return times;
     }
 }
